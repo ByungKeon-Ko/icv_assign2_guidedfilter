6eeebfe3a6aa19051a2ac7293340d4138eeffd90
ByungKeon-Ko <ccrspeed@snu.ac.kr>
2016-04-12 22:14:46 +0900

    simple updated

--- ICV_assign#2_ByungkeonKo/code/main_script.m
+++ ICV_assign#2_ByungkeonKo/code/main_script.m
@@ -1,4 +1,23 @@
 
+full_path = './../images/';
 
-fsf
+clean_file_name = dir([full_path '*_clean.png']);
+clean_img = imread(clean_file_name);
+[h,w] = size(clean_img);
+
+file_name = dir([full_path '*.png'] );
+total_images = numel(file_name);
+Nimages = numel(file_names);
+
+for j = 1:Nimages
+	m = findstr
+end
+
+
+file1 = '../../images/afghan_claen.png'
+
+tmp_im = imread(file1)
+
+imshow( tmp_in )
+
 
